cb839652f0d38f19732b6c900bc7aec6162aec5b
sbully <43850116+sbully@users.noreply.github.com>
2019-10-25 09:43:49 +0200

    Initial commit

---
+++ ModelLibrary/Contact.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLibrary
+{
+    public class Contact
+    {
+        private string firstName;
+        private string lastName;
+        private string section;
+        private string emailadr;
+        private string phoneNumber;
+        private string posteNumber;
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string Section { get => section; set => section = value; }
+        public string Emailadr { get => emailadr; set => emailadr = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+        public string PosteNumber { get => posteNumber; set => posteNumber = value; }
+
+        public Contact()
+        {
+
+        }
+
+
+    }
+}
